7262ee6be091be304ef206c8dffc5902d96004ed
Vin <ye.wen1@outlook.com>
2025-02-19 02:14:37 +0800

    refactor: config my own notion rootPageId

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '19e9d3574abd8023bae7e403e79970d9',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
